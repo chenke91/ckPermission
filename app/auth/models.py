84c21dd77bfb3e2ed35071cf77eefdb12089adc1
chenke91 <chenke91@qq.com>
2015-05-26 18:39:33 +0800

    add relationship

--- app/auth/models.py
+++ app/auth/models.py
@@ -1,12 +1,37 @@
 #encoding: utf-8
 from .. import db
 
+admin_role = db.Table(
+    'admin_role',
+    db.Column('admin_id', db.Integer, db.ForeignKey('admins.id')),
+    db.Column('role_id', db.Integer, db.ForeignKey('roles.id'))
+    )
+
 class Admin(db.Model):
     __tablename__ = 'admins'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(64), unique=True, index=True)
     name = db.Column(db.String(64), unique=True, index=True)
     permissions = db.relationship('Permission', backref='admin', lazy="dynamic")
+    roles = db.relationship('Role',
+                            secondary=admin_role,
+                            backref=db.backref('roles', lazy='dynamic'))
+
+    ##验证某个路由的权限
+    # def check_access(action):
+    #     module = Module.query.filter_by(action=action).first()
+    #     if not module:
+    #         return False
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True, index=True)
+    permissions = db.relationship('Permission', backref='role', lazy="dynamic")
+    admins = db.relationship('Admin',
+                            secondary=admin_role,
+                            backref=db.backref('admins', lazy='dynamic'))
 
 
 class Module(db.Model):
@@ -16,27 +41,13 @@ class Module(db.Model):
     name = db.Column(db.String(64), unique=True)
     pid = db.Column(db.Integer)
     is_show = db.Column(db.Boolean, default=True)
-    action = db.Column(db.String(64), index=True)
+    action = db.Column(db.String(64), unique=True, index=True)
     memo = db.Column(db.String(255))
     icon = db.Column(db.String(128))
     order = db.Column(db.Integer)
     status = db.Column(db.Integer)
     is_default = db.Column(db.Boolean, default=False)
     permissions = db.relationship('Permission', backref='module')
-
-class Role(db.Model):
-    __tablename__ = 'roles'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), unique=True, index=True)
-    permissions = db.relationship('Permission', backref='role', lazy="dynamic")
-
-class AdminRole(db.Model):
-    __tablename__ = 'admin_roles'
-
-    id = db.Column(db.Integer, primary_key=True)
-    aid = db.Column(db.Integer)
-    rid = db.Column(db.Integer)
 
 class Permission(db.Model):
     __tablename__ = 'permissions'
