f84c96a2a15ea079b94c61dc46faa535b671cdcf
chenke91 <chenke91@qq.com>
2015-05-27 18:36:37 +0800

    8421法则判定权限

--- app/auth/models.py
+++ app/auth/models.py
@@ -7,60 +7,60 @@ admin_role = db.Table(
     db.Column('role_id', db.Integer, db.ForeignKey('roles.id'))
     )
 
-permissions = db.Table(
-    'permissions',
-    db.Column('id', db.Integer, primary_key=True),
-    db.Column('admin_id', db.Integer, db.ForeignKey('admins.id')),
-    db.Column('role_id', db.Integer, db.ForeignKey('roles.id')),
-    db.Column('module_id', db.Integer, db.ForeignKey('modules.id'))
-    )
+class PermissionMixin(object):
+    #添加权限
+    def add_permission(self, module):
+        self.permissions = int(self.permissions) | int(module.permission)
+        db.session.add(self)
+        db.session.commit()
 
-class Admin(db.Model):
+    #删除权限
+    def rm_permission(self, module):
+        self.permissions = int(self.permissions) \
+            & (int(self.permissions) ^ int(module.permission))
+        db.session.add(self)
+        db.session.commit()
+
+
+class Admin(db.Model, PermissionMixin):
     __tablename__ = 'admins'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(64), unique=True, index=True)
     name = db.Column(db.String(64), unique=True, index=True)
+    permissions = db.Column(db.String(64), default=0)
     roles = db.relationship('Role',
                             secondary=admin_role,
                             backref=db.backref('admins', lazy='dynamic'),
                             lazy='dynamic')
-    modules = db.relationship('Module',
-                            secondary=permissions,
-                            backref=db.backref('admins', lazy='dynamic'),
-                            lazy='dynamic')
 
-    # 获得管理员的所有权限
-    def __whole_modules(self):
-        modules = self.modules.all()
+    #获取用户所有权限，包括自身权限与角色权限
+    def __whole_permissions(self):
+        permissions = int(self.permissions)
         for role in self.roles.all():
-            modules += role.modules.all()
-        return list(set(modules))
+            permissions |= int(role.permissions)
+        return permissions
 
     #验证某个路由的权限
     def check_access(self, action):
         module = Module.query.filter_by(action=action).first()
-        if module and module in self.__whole_modules():
-            return True
-        return False
+        if not module:
+            return False
+        whole_permissions = self.__whole_permissions()
+        return bool(whole_permissions & int(module.permission))
 
-
-class Role(db.Model):
+class Role(db.Model, PermissionMixin):
     __tablename__ = 'roles'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True, index=True)
-    modules = db.relationship('Module',
-                            secondary=permissions,
-                            backref=db.backref('roles', lazy='dynamic'),
-                            lazy='dynamic')
-
+    permissions = db.Column(db.String(64), default=0)
 
 class Module(db.Model):
     __tablename__ = 'modules'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
-    pid = db.Column(db.Integer)
+    pid = db.Column(db.Integer, default=0)
     is_show = db.Column(db.Boolean, default=True)
     action = db.Column(db.String(64), unique=True, index=True)
     memo = db.Column(db.String(255))
@@ -68,5 +68,6 @@ class Module(db.Model):
     order = db.Column(db.Integer)
     status = db.Column(db.Integer)
     is_default = db.Column(db.Boolean, default=False)
+    permission = db.Column(db.String(64), unique=True)
 
 
