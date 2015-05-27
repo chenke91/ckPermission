c185710da4cc845dd14efa6c2b577daa65eb692e
chenke91 <chenke91@qq.com>
2015-05-27 15:56:08 +0800

    add admin.check_access

--- app/auth/models.py
+++ app/auth/models.py
@@ -7,31 +7,52 @@ admin_role = db.Table(
     db.Column('role_id', db.Integer, db.ForeignKey('roles.id'))
     )
 
+permissions = db.Table(
+    'permissions',
+    db.Column('id', db.Integer, primary_key=True),
+    db.Column('admin_id', db.Integer, db.ForeignKey('admins.id')),
+    db.Column('role_id', db.Integer, db.ForeignKey('roles.id')),
+    db.Column('module_id', db.Integer, db.ForeignKey('modules.id'))
+    )
+
 class Admin(db.Model):
     __tablename__ = 'admins'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(64), unique=True, index=True)
     name = db.Column(db.String(64), unique=True, index=True)
-    permissions = db.relationship('Permission', backref='admin', lazy="dynamic")
     roles = db.relationship('Role',
                             secondary=admin_role,
-                            backref=db.backref('roles', lazy='dynamic'))
+                            backref=db.backref('admins', lazy='dynamic'),
+                            lazy='dynamic')
+    modules = db.relationship('Module',
+                            secondary=permissions,
+                            backref=db.backref('admins', lazy='dynamic'),
+                            lazy='dynamic')
 
-    ##验证某个路由的权限
-    # def check_access(action):
-    #     module = Module.query.filter_by(action=action).first()
-    #     if not module:
-    #         return False
+    # 获得管理员的所有权限
+    def __whole_modules(self):
+        modules = self.modules.all()
+        for role in self.roles.all():
+            modules += role.modules.all()
+        return list(set(modules))
+
+    #验证某个路由的权限
+    def check_access(self, action):
+        module = Module.query.filter_by(action=action).first()
+        if module and module in self.__whole_modules():
+            return True
+        return False
+
 
 class Role(db.Model):
     __tablename__ = 'roles'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True, index=True)
-    permissions = db.relationship('Permission', backref='role', lazy="dynamic")
-    admins = db.relationship('Admin',
-                            secondary=admin_role,
-                            backref=db.backref('admins', lazy='dynamic'))
+    modules = db.relationship('Module',
+                            secondary=permissions,
+                            backref=db.backref('roles', lazy='dynamic'),
+                            lazy='dynamic')
 
 
 class Module(db.Model):
@@ -47,13 +68,5 @@ class Module(db.Model):
     order = db.Column(db.Integer)
     status = db.Column(db.Integer)
     is_default = db.Column(db.Boolean, default=False)
-    permissions = db.relationship('Permission', backref='module')
 
-class Permission(db.Model):
-    __tablename__ = 'permissions'
-
-    id = db.Column(db.Integer, primary_key=True)
-    aid = db.Column(db.Integer, db.ForeignKey('admins.id'))
-    rid = db.Column(db.Integer, db.ForeignKey('roles.id'))
-    mid = db.Column(db.Integer, db.ForeignKey('modules.id'))
 
